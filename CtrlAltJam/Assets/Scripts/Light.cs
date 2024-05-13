c2afae7e9eae64eb7ff39a6ebb3c4bb7468efc73
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2024-05-13 13:50:47 -0300

    Sistema das luzes

---
+++ CtrlAltJam/Assets/Scripts/Light.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light : MonoBehaviour
+{
+    [SerializeField] bool aceso = false;
+
+
+    //FIXME: Isso é apenas um teste
+    SpriteRenderer sp;
+
+    private void Start()
+    {
+        sp = GetComponent<SpriteRenderer>();
+        sp.enabled = aceso;
+    }
+
+
+    public void AcenderApagar()
+    {
+        aceso = !aceso;
+        sp.enabled = aceso;
+    }
+
+}
---
+++ CtrlAltJam/Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    [SerializeField] GameObject e;
+    bool active = false;
+
+    public delegate void MyDelegate();
+    MyDelegate ligarApagar;
+
+    [SerializeField] Light[] luzes;
+
+    private void Start()
+    {
+        foreach(Light l in luzes)
+        {
+            ligarApagar += l.AcenderApagar;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!active)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.E) && ligarApagar != null)
+        {
+            ligarApagar();
+        }
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            e.SetActive(true);
+            active = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            e.SetActive(false);
+            active = false;
+        }
+    }
+}
