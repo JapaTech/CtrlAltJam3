df27c7a90d92251196c66a046b8ed72af72ce518
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2024-05-13 13:16:05 -0300

    Movimentação do player

---
+++ CtrlAltJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed, jump;
+    Rigidbody2D rb;
+    float input;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(input * speed, rb.velocity.y);
+    }
+}
